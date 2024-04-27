5b098298772d39b5de4b30ef9903993e575f4661
Nicholas Berlette <nick@berlette.com>
2024-04-26 20:06:49 -0700

    chore: update docs and coverage

--- docs/search_index.js
+++ docs/search_index.js
@@ -1,3 +1,3 @@
 (function () {
-  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["function","typeAlias"],"name":"clsx","file":".","location":{"filename":"default","line":141,"col":0,"byteIndex":4354},"declarationKind":"export","deprecated":false}]};
+  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["function","typeAlias"],"name":"clsx","file":".","location":{"filename":"","line":142,"col":0,"byteIndex":4375},"declarationKind":"export","deprecated":false}]};
 })()
